eca201f90cb890dbee7ec3ada8e2c5208df9df76
manitu85 <manitu.psy@gmail.com>
2019-10-03 17:08:38 +0200

    #

---
+++ src/Components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaGithub }from 'react-icons/fa'
+import PropTypes from 'prop-types'
+
+
+const Navbar = ({title}) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1><FaGithub /> {title}</h1>
+      {/* <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul> */}
+    </nav>
+  )
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Navbar
---
+++ src/Components/users/UserItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserItem extends Component {
+  render() {
+    return (
+      <div>
+        User item
+      </div>
+    )
+  }
+}
+
+export default UserItem
